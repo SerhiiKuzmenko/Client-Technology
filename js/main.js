8deb8b74cf2941905bb75df368ac675027ca21e8
KuzmenkoDev <110684653+SerhiiKuzmenko@users.noreply.github.com>
2024-02-09 22:53:02 +0200

    Add modal windows

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+const modalBtn = document.querySelector("#registration-form");
+const regForm = document.querySelector("#modal");
+const spanX = document.querySelectorAll(".exit")[0];
+const spanX1 = document.querySelectorAll(".exit")[1];
+
+const loginFormBtn = document.querySelector("#login-form");
+const logForm = document.querySelector("#login-modal");
+
+
+
+modalBtn.addEventListener("click", ()=>{
+    regForm.style.display="block";
+});
+
+window.addEventListener("click", (event)=>{
+    if(event.target==regForm){
+        regForm.style.display="none";
+    }
+});
+
+spanX.addEventListener("click", ()=>{
+    regForm.style.display="none";
+});
+
+spanX1.addEventListener("click", ()=>{
+    logForm.style.display="none";
+});
+
+loginFormBtn.addEventListener("click", ()=>{
+    logForm.style.display="block";
+});
